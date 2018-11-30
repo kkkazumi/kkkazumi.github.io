47e79e6cd467e8f9311bab5eeb0eb69da98b4af0
kumagai <kumagai@mizuuchi.lab.tuat.ac.jp>
2018-11-30 23:28:02 +0900

    add render photo

--- main.js
+++ main.js
@@ -68,3 +68,15 @@ Asset._loadImage = function(asset, onLoad) {
   image.onload = onLoad;
   Asset.images[asset.name] = image;
 };
+
+
+function render() {
+  // 画面全体をクリア
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // 背景を表示
+  ctx.drawImage(Asset.images['back'], 0, 0);
+
+  // みかん箱を表示
+  ctx.drawImage(Asset.images['box'], 0, 0);
+}
